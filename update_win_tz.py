37f1bf1399cea84f85092a180e815f343457cf4b
Sergey Shlyapugin <shlyapugin@gmail.com>
2024-04-28 19:53:14 +0300

    lxml -> defusedxml

--- update_win_tz.py
+++ update_win_tz.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 from urllib.request import urlopen
 
-from lxml import etree
+from defusedxml import ElementTree
 
 
 ZONES_XML_URL = 'https://raw.githubusercontent.com/unicode-org/cldr/main/common/supplemental/windowsZones.xml'
 OUTPUT_FILE = 'pymstodo/windows_zones_data.py'
 
 with urlopen(ZONES_XML_URL) as response:  # noqa: S310
-    zones_xml = etree.parse(response)  # noqa: S320
+    zones_xml = ElementTree.parse(response)
 
 tz_data = {}
-for zone in zones_xml.xpath('//mapZone'):
+for zone in zones_xml.getroot().find('.//windowsZones/mapTimezones'):
     attrib = zone.attrib
     if attrib['territory'] == '001':
         tz_data[attrib['other']] = attrib['type']
 
 Path(OUTPUT_FILE).write_text(f'windows_zones = {tz_data}\n')
-
